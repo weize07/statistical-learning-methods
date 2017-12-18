df321d778a1d12e5fffa61c1805981d7e364766e
weize <ze.wei1989@gmail.com>
2017-12-18 23:06:26 +0800

    dt based on id3

---
+++ decision_tree/id3_decision_tree.py
@@ -0,0 +1,13 @@
+#encoding=utf-8
+class ID3DecisionTree(object):
+    def __init__(self, inputs, labels, info_gain_thres):
+        self.training_inputs = inputs
+        self.training_labels = labels
+        self.info_gain_thres = info_gain_thres
+
+    def train(self):
+
+
+    def predict(self, input):
+        
+    
